894ee1df76edb684a12bc9403def0cc998a06efa
André Mourão <andre.mourao@fccn.pt>
2020-02-13 15:43:58 +0000

    Fixed very dumb mistake in the PageImageData comparator.

--- src/main/java/data/PageImageDataComparator.java
+++ src/main/java/data/PageImageDataComparator.java
@@ -6,8 +6,8 @@ public class PageImageDataComparator implements Comparator<PageImageData> {
 
     @Override
     public int compare(PageImageData t1, PageImageData t2) {
-        if (t1.getTimestamp().equals(t2)){
-            return t1.getPageURL().length() - t1.getPageURL().length();
+        if (t1.getTimestamp().equals(t2.getTimestamp())){
+            return t1.getPageURL().length() - t2.getPageURL().length();
         } else {
             return t1.getTimestamp().compareTo(t2.getTimestamp());
         }
