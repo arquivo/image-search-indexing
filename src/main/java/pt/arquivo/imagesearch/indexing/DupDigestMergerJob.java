bd65ea148bcc722dca8910f2f77e594716c89210
André Mourão <andre.mourao@fccn.pt>
2021-06-30 19:19:18 +0100

    Comment DupDigestMergerJob

--- src/main/java/pt/arquivo/imagesearch/indexing/DupDigestMergerJob.java
+++ src/main/java/pt/arquivo/imagesearch/indexing/DupDigestMergerJob.java
@@ -27,15 +27,43 @@ import pt.arquivo.imagesearch.indexing.processors.ImageInformationMerger;
 
 import java.io.IOException;
 
+/**
+ * Hadoop process responsable for the 2nd stage of the pipeline.
+ * Takes intermediate, duplicate results from the first stage (grouped by digest), and performs digest-based deduplication.
+ * The output is a set of JSONL files ready for the NSFW classification process
+ */
 public class DupDigestMergerJob extends Configured implements Tool {
 
+
+    /**
+     * OUTPUT_MODE corresponds to the way the data will be represented in the output JSON. In this scenario, the same image is indexed multiple times, each one with a set of page metadata matching a different page where it showed up.
+     * The FULL mode corresponds to having a JSON document per page where the image appears.
+     * The COMPACT mode (currently used in prod) creates a single JSON document per image, with the oldest page metadata plus all unique image metadata (imgTitle, imgAlt, imgCaption) from all pages.
+     * More information about the process behind this decision is present in the technical report
+     */
     public enum OUTPUT_MODE {
         FULL,
         COMPACT
     }
 
+
+    /**
+     * Hadoop config key for the output mode config
+     */
     public static final String OUTPUT_MODE_NAME = "output_mode";
 
+
+    /**
+     * Counters for the map process:
+     * <p>
+     * RECORDS_IN: Count of images (with unique digests) that where processed
+     * RECORDS_OUT: Count of images that where outputed
+     * RECORDS_WITH_METADATA: Number of images that have image specific metadata
+     * RECORDS_WITHOUT_METADATA: Number of images that don't have image specific metadata
+     * URL_IMAGES_PAGESALL: Number of pages referencing images (without deduplication)
+     * URL_IMAGESALL_PAGES: Number of images referenced by pages (without deduplication)
+     * URL_IMAGES_PAGES: Number of images processed (with deduplication)
+     */
     public enum COUNTERS {
         RECORDS_MAP_IN,
         RECORDS_IN,
@@ -48,6 +76,13 @@ public class DupDigestMergerJob extends Configured implements Tool {
 
     }
 
+    /**
+     * Counters for the reduce process:
+     * <p>
+     * URL_IMAGES_PAGESALL: Number of pages referencing images (without deduplication)
+     * URL_IMAGESALL_PAGES: Number of images referenced by pages (without deduplication)
+     * URL_IMAGES_PAGES: Number of images processed (with deduplication)
+     */
     public enum REDUCE_COUNTERS {
         URL_IMAGES_PAGESALL,
         URL_IMAGESALL_PAGES,
@@ -58,6 +93,14 @@ public class DupDigestMergerJob extends Configured implements Tool {
 
         private final Logger logger = Logger.getLogger(Map.class);
 
+
+        /**
+         * Map process groups FullImageMetadata records by digest by writting them to the "key" Hadoop entry
+         *
+         * @param key     image digest for this FullImageMetadata object
+         * @param value   single FullImageMetadata object to be deduplicated
+         * @param context Hadoop context
+         */
         public void map(Text key, Writable value, Context context) {
             try {
                 context.getCounter(COUNTERS.RECORDS_MAP_IN).increment(1);
@@ -80,11 +123,17 @@ public class DupDigestMergerJob extends Configured implements Tool {
             merger = new ImageInformationMerger(context);
             Configuration config = context.getConfiguration();
             String legacyOutput = config.get(OUTPUT_MODE_NAME);
-
             outputMode = OUTPUT_MODE.valueOf(legacyOutput);
         }
 
 
+        /**
+         * For each digest, merges all FullImageMetadata into a single record for output
+         *
+         * @param key     image digest for this set of FullImageMetadata object
+         * @param values  set of FullImageMetadata object to be deduplicated
+         * @param context Hadoop context
+         */
         public void reduce(Text key, Iterable<Writable> values, Context context) {
             logger.debug("Reducing: " + key);
 
@@ -110,6 +159,12 @@ public class DupDigestMergerJob extends Configured implements Tool {
 
         }
 
+        /**
+         * Transforms a FullImageMetadata object into the desired JSONL format
+         *
+         * @param context Hadoop context
+         * @param result FullImageMetadata to be outputted
+         */
         private void exportToJson(Reducer<Text, Writable, NullWritable, Text>.Context context, FullImageMetadata result) {
             Gson gson = new GsonBuilder()
                     .registerTypeAdapter(MultiPageImageData.class, new MultiPageImageDataSerializer())
@@ -117,16 +172,21 @@ public class DupDigestMergerJob extends Configured implements Tool {
                     .create();
 
             try {
+                // If output mode is FULL, "deduplication" only removes FullImageMetadata if they are exactly the same. There may be multiple image and page records for each unique (by digest) image
                 if (outputMode == OUTPUT_MODE.FULL) {
                     for (ImageData data : result.getImageDatasValues())
                         context.write(NullWritable.get(), new Text(gson.toJson(data)));
                     for (PageImageData data : result.getPageImageDatasValues())
                         context.write(NullWritable.get(), new Text(gson.toJson(data)));
-                } else { // if (outputMode == OUTPUT_MODE.COMPACT) {
-                    if (!result.getPageImageDatas().isEmpty() && !result.getImageDatas().isEmpty()){
+                // If output mode is COMPACT, "deduplication" will make that there will only be a single image data line and a single image metadata line
+                } else if (outputMode == OUTPUT_MODE.COMPACT) {
+                    if (!result.getPageImageDatas().isEmpty() && !result.getImageDatas().isEmpty()) {
                         ImageData id = result.getImageDatas().firstKey();
                         MultiPageImageData pid = new MultiPageImageData(result);
+                        // ImageData and MultiPageImageData are written separately to simplify the next processing stage
+                        // The ImageData JSON contains a base64 version of the iamge and will be used for NSFW classification
                         context.write(NullWritable.get(), new Text(gson.toJson(id)));
+                        // The MultiPageImageData contains image and page metadata and is the document that is sent for SolrCloud
                         context.write(NullWritable.get(), new Text(gson.toJson(pid)));
                     }
 
@@ -137,6 +197,13 @@ public class DupDigestMergerJob extends Configured implements Tool {
         }
     }
 
+    /**
+     * Runs the DupDigestMergerJob process
+     *
+     * @param args: args[0]: collection name, args[1]: files per map, args[2]: number of reduces, args[4]: output mode (FULL, COMPACT), (optional) args[4]: input HDFS dir,  args[5]: output HDFS dir
+     * @return Whether Hadoop process finihsed sucessfully (0) or not (1)
+     * @throws Exception if Exceptions may reach this stage, there is not much one can do
+     */
     @Override
     public int run(String[] args) throws Exception {
         assert args.length >= 1 : "Missing collection name argument";
@@ -149,7 +216,7 @@ public class DupDigestMergerJob extends Configured implements Tool {
         assert args.length >= 3 : "Missing number of reduces";
         int reducesCount = Integer.parseInt(args[2]);
 
-        assert args.length >= 4 : "Missing Output mode (e.g. legacy, full, compact)";
+        assert args.length >= 4 : "Missing Output mode (FULL, COMPACT)";
         String outputModeString = args[3];
 
         String inputDir;
@@ -160,7 +227,7 @@ public class DupDigestMergerJob extends Configured implements Tool {
         conf.set(OUTPUT_MODE_NAME, outputModeString);
         FileSystem hdfs = FileSystem.get(conf);
 
-        if (args.length >= 6){
+        if (args.length >= 6) {
             inputDir = args[4];
             outputDirDigest = args[5];
         } else {
@@ -219,7 +286,7 @@ public class DupDigestMergerJob extends Configured implements Tool {
         //job.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", linespermap);
         //job.getConfiguration().setInt("mapreduce.job.running.map.limit", maxMaps); /*Maximum simultaneous maps running*/
 
-        return jobDigest.waitForCompletion(true)? 0: 1;
+        return jobDigest.waitForCompletion(true) ? 0 : 1;
     }
 
 
