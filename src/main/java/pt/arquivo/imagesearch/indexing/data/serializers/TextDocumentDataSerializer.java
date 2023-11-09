2cedd36ef0f871c9217990ba2fe0df56a6b8cc27
abmou <andre.b.mourao+arquivo@gmail.com>
2023-11-09 15:11:38 +0000

    Remove unneeded fields from output

--- src/main/java/pt/arquivo/imagesearch/indexing/data/serializers/TextDocumentDataSerializer.java
+++ src/main/java/pt/arquivo/imagesearch/indexing/data/serializers/TextDocumentDataSerializer.java
@@ -4,25 +4,8 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-import pt.arquivo.imagesearch.indexing.data.MultiPageImageData;
 import pt.arquivo.imagesearch.indexing.data.TextDocumentData;
-import pt.arquivo.imagesearch.indexing.processors.ImageInformationExtractor;
-import pt.arquivo.imagesearch.indexing.utils.ImageSearchIndexingUtil;
-import pt.arquivo.imagesearch.indexing.utils.WARCInformationParser;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.lang.reflect.Type;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Configures JsonSerializable to export the object in the desired JSON format
@@ -58,14 +41,14 @@ public class TextDocumentDataSerializer implements JsonSerializer<TextDocumentDa
         obj.addProperty("warc", src.getWarc());
         obj.addProperty("warcOffset", src.getWarcOffset());
         obj.addProperty("surt", src.getSurt());
-        obj.addProperty("protocol", src.getProtocol());
+        //obj.addProperty("protocol", src.getProtocol());
         obj.addProperty("urlHash", src.getURLHash());
         obj.addProperty("collection", src.getCollection());
         obj.add("outlinks", context.serialize(src.getOutlinks()));
         
-        obj.addProperty("safe", 0);
-        obj.addProperty("spam", 0);
-        obj.addProperty("blocked", 0);
+        //obj.addProperty("safe", 0);
+        //obj.addProperty("spam", 0);
+        //obj.addProperty("blocked", 0);
 
         return obj;
     }
